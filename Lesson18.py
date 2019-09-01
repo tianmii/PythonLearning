711a9c604ac4342372af457657f2dd3dc050612a
Tammy Lee <tian.mii@gmail.com>
2019-09-01 14:34:54 -0700

    in-progress

--- Lesson18.py
+++ Lesson18.py
@@ -1,39 +1,39 @@
-############ Dictionary #######################
+# -------------------Dictionary----------------------
 # Stores info that are connected in some way, connected with key-value pairs
 
 # General format, be mindful of what you name the first two variables
 # for key_name, value_name in dictionary_name.items():
-    ##### The key  is stored in whatever you called the first variable
-    # print(key_name)
-    ##### The value associated with that key is stored in your second variable
-    # print(value_name)
+# The key  is stored in whatever you called the first variable
+# print(key_name)
+# The value associated with that key is stored in your second variable
+# print(value_name)
 
 python_words = {'list': 'A collection of values that are not connected, but have an order.',
                 'dictionary': 'A collection of key-value pairs.',
                 'function': 'A named set of instructions that defines a set of actions in Python.',
-}
+                }
 
-############ Print out the items in the dictionary.
+# Print out the items in the dictionary.
 for word, meaning in python_words.items():
     print("\nWord: {word}".format(word=word))
     print("Meaning: {meaning}".format(meaning=meaning))
 
-
-
-###########
+# ---------------------------------------------
 game_words = {'gore magala': 'The eyes have not developed and relies on the Frenzy Virus to hunt.',
-                'zinogre': 'The spikes on its body lie flat, but when it has a built up charge, they stick out vertically into the air.',
-                'dodogama': 'A stout wyvern that uses its strong lower jaw to scoop rocks and compress them into lava rocks.'
-}
+              'zinogre': 'The spikes on its body lie flat, but when it has a built up charge, they stick out '
+                         'vertically into the air.',
+              'dodogama': 'A stout wyvern that uses its strong lower jaw to scoop rocks and compress them into lava '
+                          'rocks. '
+              }
 
 for word, description in game_words.items():
     print("\nMonster: {monster}".format(monster=word.title()))
     print("Description: {sentence}".format(sentence=description))
 
-## empty dictionary
+# empty dictionary
 animal_facts = {}
 
-#Fill fictionary, pair by pair
+# Fill fictionary, pair by pair
 animal_facts['millipedes'] = 'They are roly polies and danger noodles combined into one.'
 animal_facts['bats'] = 'Bats are bugs.'
 animal_facts['martins'] = 'Super cute ferrets.'
@@ -42,16 +42,17 @@ for animal, fact in animal_facts.items():
     print("\nAnimal: {animal}".format(animal=animal.title()))
     print("Fact: {fact}".format(fact=fact))
 
-###########
-#### USE DEF AND LABEL THEM, NO GLOBAL
-##########
+
+# ------------------------------------
+# USE DEF AND LABEL THEM, NO GLOBAL
+# -----------------------------------
 
 def show_game_facts(game_facts):
-# This function takes in a dictionary of animal names and facts
-# prints each word with meaning
+    # This function takes in a dictionary of animal names and facts
+    # prints each word with meaning
     print("\nThese are the animal facts I know:")
     for game, fact in game_facts.items():
-        # THIS PART IS MUCH CLEANER vs ealier typing out the literal words like
+        # THIS PART IS MUCH CLEANER vs earlier typing out the literal words like
         # Animal: Bats, Fact: They fly VERSUS
         # Bats: They fly
         print("\n{game}: {fact}".format(
@@ -60,18 +61,20 @@ def show_game_facts(game_facts):
 
 
 game_facts = {'gore magala': 'The eyes have not developed and relies on the Frenzy Virus to hunt.',
-                'zinogre': 'The spikes on its body lie flat, but when it has a built up charge, they stick out vertically into the air.',
-                'dodogama': 'A stout wyvern that uses its strong lower jaw to scoop rocks and compress them into lava rocks.'
-             }
+              'zinogre': 'The spikes on its body lie flat, but when it has a built up charge, they stick out '
+                         'vertically into the air.',
+              'dodogama': 'A stout wyvern that uses its strong lower jaw to scoop rocks and compress them into lava '
+                          'rocks. '
+              }
 show_game_facts(game_facts)
 
-##### Remove word 'dodogama' and facts from dictionary
+# Remove word 'dodogama' and facts from dictionary
 del game_facts['dodogama']
 
 show_game_facts(game_facts)
 
 #############################
-#### MODIFYING KEYS is trickier
+# MODIFYING KEYS is trickier
 # Make a new key, copy and assign same value
 # delete old key and also deletes old value
 
@@ -83,17 +86,17 @@ game_facts = {'RAFoLOS': 'Shows up in every Monster Hunter game.'}
 game_facts['Rathalos'] = game_facts['RAFoLOS']
 del game_facts['RAFoLOS']
 
-#print dictionary show key changed
+# print dictionary show key changed
 print(game_facts)
-#################################################
+################################################
 
-###LOOPING THROUGH ALL KEY VALUE PAIRS
+# LOOPING THROUGH ALL KEY VALUE PAIRS
 ###########
 
 my_dictionary = {'key_1': 'value_1',
-                'key_2': 'value_2',
-                'key_3': 'value_3',
-}
+                 'key_2': 'value_2',
+                 'key_3': 'value_3',
+                 }
 
 for key, value in my_dictionary.items():
     print("\n{new_key}: {new_key}".format(new_key=key))
