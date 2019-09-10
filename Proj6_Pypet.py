b779cb12c6dc35f34ecea06ff279f4bce07a644c
Tammy Lee <tian.mii@gmail.com>
2019-09-09 18:09:54 -0700

    in progress

---
+++ Proj6_Pypet.py
@@ -0,0 +1,15 @@
+# Pypet
+# Tamogotchi like game
+
+print("Welcome to Pypet")
+
+# string, has ""
+name = "JoJo"
+# integer b/c whole number, no ""
+age = 1
+# float b/c decimals, no ""
+weight = 4.1
+# Boolean, no ""
+hungry = True
+
+
