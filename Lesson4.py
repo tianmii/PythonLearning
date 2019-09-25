7ce45c34bd6108df5c3b1a5cad9de29126ab4fc7
Tammy Lee <tian.mii@gmail.com>
2019-09-25 12:05:16 -0700

    in progress

--- Lesson4.py
+++ Lesson4.py
@@ -5,18 +5,18 @@
 
 game = 'zelda'
 sentence = f"I like the game {game}"
-
+print()
 #
 games = ['zelda', 'pokemon', 'kirby']
 for word in games:
     print('I like the game {}, {}'.format(
         word, 1
     ))
-
+print()
 # for word in games:
 for word in ['zelda', 'pokemon', 'kirby']:
-    print(f'\nI like the game {word.lower()}')
-
+    print(f'I like the game {word.lower()}')
+print()
 # messed up version
 # for word in enumerate(['zelda', 'pokemon', 'kirby']):
 # print('I like the game {a}'.format(
@@ -27,7 +27,7 @@ for word in ['zelda', 'pokemon', 'kirby']:
 # how to loop based on enumerate function using for ___, in enumerate([strings]):
 for idx, word in enumerate(['zelda', 'pokemon', 'kirby']):
     print(f'{idx}. I like the game {word}')
-
+print()
 #
 for idx, word in enumerate(['zelda', 'pokemon', 'kirby'], start=12):
     print(f'{idx}. I like the game {word}')
@@ -38,38 +38,31 @@ for tup in enumerate(['zelda', 'pokemon', 'kirby']):
     print(tup)
     idx = tup[0]
     word = tup[1]
-    print('{number}. I like the game {a}'.format(
-        a=word,
-        number=idx
-    ))
+    print(f'{idx}. I like the game {word}')
 print()
 
 #
 for idx, word, *_ in [('zelda', 5, 12), ('pokemon', 9, 14, 80, 100, 3333), ('kirby', 100, 200, 300)]:
-    print('{number}. I like the game {a}.'.format(
-        a=word,
-        number=idx,
-        # that=thing,
-    ))
+    print(f'{idx}. I like the game {word}.')
+    # that=thing,
 
+print()
 # different way of writing the list {} does not have to have a word in it
 # as long as the format ordering is correct
 for idx, word in enumerate(['zelda', 'pokemon', 'kirby']):
     print('{}. I like the game {}'.format(
         idx, word
     ))
-
+print()
 #
-for x, y in enumerate(['ciri', 'geralt', 'dandelion'], start=1):
-    print('{} is the ranking of {}'.format(
-        x, y.title()
-    ))
-
+for num, name in enumerate(['ciri', 'geralt', 'dandelion'], start=1):
+    print(f'{num} is the ranking of {name.title()}')
+print()
 #
 people = ['link', 'zelda', 'sidon']
 print('sidon' in people)
 print('ganondorf' in people)
-
+print()
 #
 # people = ['link', 'sidon', 'zelda']
 # people.append('epona')
@@ -77,16 +70,14 @@ print('ganondorf' in people)
 # for people in enumerate(['link', 'sidon', 'zelda']):
 
 dags = ['doge', 'shiba', 'pupper']
-dags.append('gabe')
+dags.append('moonmoon')
 for dag in dags:
-    print(dag.title() + "s are cool.")
-
+    print(f"{dag.title()}s are cool.")
+print()
 #
 # old example in lesson 3
 for word in ['zelda', 'pokemon', 'kirby']:
-    print('I like the game {a}'.format(
-        a=word.lower()
-    ))
+    print(f'I like the game {word.lower()}')
 
 game = 'zelda'
 sentence = "I like the game {a}".format(
