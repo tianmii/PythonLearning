19332a4c073e2b0544ae30c4292eea3a6fcc5ac1
Tammy Lee <tian.mii@gmail.com>
2019-12-09 09:01:09 -0800

    in progress

--- SampleProjects/WebScraping/Amazon_wishlist.py
+++ SampleProjects/WebScraping/Amazon_wishlist.py
@@ -1,22 +1,25 @@
 from selenium import webdriver
 import time
+
 driver = webdriver.Chrome()
 driver.get("https://www.amazon.com/hz/wishlist/ls/1D7C42EMM4RMP?ref_=wl_share")
 
-
 try:
-    item_sale = driver.find_elements_by_class_name("itemPriceDrop")
-    if item_sale is True:
-        for sale in item_sale:
-            print(sale.text)
+    
+    item_sales = driver.find_elements_by_class_name("itemPriceDrop")
+    item_names = driver.find_elements_by_class_name("a-size-small")
+    # Don't need if item_sale is True:, doesn't print
+    if item_sales:
+        for name in item_names:
+            print(f"{name.text}\n")
+        # for sale in item_sale:
+        #     print(sale.text)
     else:
         print("No price drops today.")
 finally:
     time.sleep(2)
     print("Test Complete")
     driver.close()
-
-
 
 # current_price = driver.find_element_by_id("itemPrice_I18IH1YAURA6D3")
 # print(current_price.text)
@@ -30,21 +33,18 @@ finally:
 #         print(f"{item.text}\n")
 
 
-
 # try:
 #     item_price = driver.find_elements_by_class_name("a-text-bold")
 #     for item in item_price:
 #         print(f"{item.text}\n")
-    # item_name = driver.find_elements_by_class_name("a-link-normal")
+# item_name = driver.find_elements_by_class_name("a-link-normal")
 
-    # for name in item_name:
-    #     print(f"{name.text}")
+# for name in item_name:
+#     print(f"{name.text}")
 
-    # item_price = driver.find_elements_by_class_name("a-price")
-    # for price in item_price:
-    #     print(f"{price.text}\n")
-
-
+# item_price = driver.find_elements_by_class_name("a-price")
+# for price in item_price:
+#     print(f"{price.text}\n")
 
 
 # price_dropped = driver.find_elements_by_xpath("//*[@id='itemInfo_IXCN0JX27H5HG']")
--- SampleProjects/WebScraping/Amazon_wishlist2.py
+++ SampleProjects/WebScraping/Amazon_wishlist2.py
@@ -4,20 +4,43 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://www.amazon.com/hz/wishlist/ls/1D7C42EMM4RMP?ref_=wl_share")
 
-if True:
-    try:
-        item_sale = driver.find_elements_by_class_name("itemPriceDrop")
-        item_name = driver.find_elements_by_class_name("a-list-item")
 
-        for sale in item_sale:
-            print(sale.text)
+try:
+    parent_class = driver.find_element_by_id("item-page-wrapper")
+    item_names = parent_class.find_elements_by_class_name("a-list-item")
+    item_sales = parent_class.find_elements_by_class_name("itemPriceDrop")
+    if item_sales:
+        for name in item_names:
+            print(f"{name.text}\n")
+
+    # print(parent_class.text)
+
+finally:
+    time.sleep(2)
+    print("Test Complete")
+    driver.close()
+# if True:
+#     try:
+#         # item_sale = driver.find_elements_by_class_name("itemPriceDrop")
+#         item_name = driver.find_elements_by_class_name("a-section")
+#         for name in item_name:
+#             print(name.text)
+#         # for sale in item_sale:
+#         #     print(sale.text)
+#     finally:
+#         time.sleep(2)
+#         print("Test Complete")
+#         driver.close()
 
 
 
-    finally:
+# try:
+#     urls = driver.find_elements_by_tag_name("a").get_attribute('href')
+#     for url in urls:
+#         print(url.text)
+    # item_sale = driver.find_elements_by_class_name("itemPriceDrop")
 
-        time.sleep(2)
-        print("Test Complete")
-        driver.close()
 
+    # for sale in item_sale:
+    #     print(sale.text)
 
