30a180842ed14763fb578466e6a78cea75d5a39b
Tammy Lee <54225567+tianmii@users.noreply.github.com>
2019-12-13 10:24:02 -0800

    Revert "in progress"
    This reverts commit 4ed8ff048afdfc7f6090fc46386216e4ba360901.

--- SampleProjects/WebScraping/Amazon_wishlist_ff.py
+++ SampleProjects/WebScraping/Amazon_wishlist_ff.py
@@ -10,11 +10,10 @@ def popup_time_wait():
 
 
 def scroll_time():
-    scroll_pause_time = 3
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
         driver.execute_script("window.scrollTo(0 ,document.body.scrollHeight);")
-        time.sleep(scroll_pause_time)
+        time.sleep(3)
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
